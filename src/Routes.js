0691170965e8c66a145ce41dc2fc503aa206ef1e
Ajmal Jalal <ajmaljalal58@gmail.com>
2018-09-28 00:14:12 -0700

    app structure completed

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Resources/css/app.css';
+import Layout from './hoc/Layout';
+
+const Routes = (props) => {
+    return (
+      <Layout>
+        Child
+      </Layout>
+    );
+}
+
+export default Routes;
